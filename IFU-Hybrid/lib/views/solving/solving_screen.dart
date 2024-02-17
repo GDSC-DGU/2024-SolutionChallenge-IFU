2d695e0d06ac30e227f1a25b3397222ccf113e1f
kurtyoon <akakertcobain@naver.com>
2024-02-17 10:57:28 +0100

    [Feat] define solving screen base container

--- IFU-Hybrid/lib/views/solving/solving_screen.dart
+++ IFU-Hybrid/lib/views/solving/solving_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:ifu/utils/class/app_routes.dart';
 import 'package:ifu/view_models/solving/solving_view_model.dart';
 import 'package:ifu/views/base/base_screen.dart';
 import 'package:ifu/widgets/base/default_back_appbar.dart';
@@ -8,7 +10,33 @@ class SolvingScreen extends BaseScreen<SolvingViewModel> {
 
   @override
   Widget buildBody(BuildContext context) {
-    return Container();
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Center(),
+          TextButton(
+              style: TextButton.styleFrom(
+                padding: const EdgeInsets.symmetric(horizontal: 130, vertical: 15),
+                backgroundColor: const Color(0xFFF2F4FA),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15)
+                ),
+              ),
+              onPressed: () {
+                Get.toNamed(Routes.SOLUTION);
+              },
+              child: const Text(
+                  'Solution',
+                  style: TextStyle(
+                      color: Color(0xFF2B90D9),
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold
+                  )
+              )
+          )
+        ]
+      )
+    );
   }
 
   @override
